80559a1fe3bf3f4a9113bedeaff960ee7b48ebd9
fshofiquel <fshofique@gmail.com>
2021-07-20 14:46:59 -0400

    Initial implementation of multiples for application (save as and load window)
    [INCOMPLETE]The user shall interact with the application through a Graphical User Interface
    
    [INCOMPLETE]The user shall be able to store at least 100 inventory items
    Each inventory item shall have a value representing its monetary value in US dollars
    
    [INCOMPLETE]Each inventory item shall have a unique serial number in the format of XXXXXXXXXX where X can be either a letter or digit
    
    [INCOMPLETE]Each inventory item shall have a name between 2 and 256 characters in length (inclusive)
    
    [INCOMPLETE]The user shall be able to add a new inventory item
    
    [INCOMPLETE]The application shall display an error message if the user enters an existing serial number for the new item
    
    [INCOMPLETE]The user shall be able to remove an existing inventory item
    
    [INCOMPLETE]The user shall be able to edit the value of an existing inventory item
    
    [INCOMPLETE]The user shall be able to edit the serial number of an existing inventory item
    
    [INCOMPLETE]The application shall prevent the user from duplicating the serial number
    
    [INCOMPLETE]The user shall be able to edit the name of an existing inventory item
    
    [INCOMPLETE]The user shall be able to sort the inventory items by value
    
    [INCOMPLETE]The user shall be able to sort inventory items by serial number
    
    [INCOMPLETE]The user shall be able to sort inventory items by name
    
    [INCOMPLETE]The user shall be able to search for an inventory item by serial number
    
    [INCOMPLETE]The user shall be able to search for an inventory item by name
    
    [INCOMPLETE]The user shall be able to save their inventory items to a file
    
    [INCOMPLETE]The user shall be able to select the file format from among the following set of options: TSV (tab-separated value), HTML, JSON
    
    [INCOMPLETE]TSV files shall shall list one inventory item per line, separate each field within an inventory item using a tab character, and end with the extension .txt
    
    [INCOMPLETE]HTML files shall contain valid HTML and end with the extension .html
    
    [INCOMPLETE]JSON files shall contain valid JSON and end with the extension .json
    
    [INCOMPLETE]The user shall provide the file name and file location of the file to save
    
    [INCOMPLETE]The user shall be able to load inventory items from a file that was previously created by the application.
    
    [INCOMPLETE]The user shall provide the file name and file location of the file to load

---
+++ src/main/java/ucf/assignments/SaveFileAsController.java
@@ -0,0 +1,24 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class SaveFileAsController
+{
+	public ChoiceBox saveAsChoiceBox;
+	public TextField filePathTextField;
+
+	public void saveFormatClick(MouseEvent mouseEvent)
+	{
+	}
+
+	public void saveAsClick(ActionEvent actionEvent)
+	{
+	}
+
+	public void loadFileClick(ActionEvent actionEvent)
+	{
+	}
+}
--- src/main/java/ucf/assignments/TrackingInventoryController.java
+++ src/main/java/ucf/assignments/TrackingInventoryController.java
@@ -5,6 +5,22 @@
 
 package ucf.assignments;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class TrackingInventoryController
 {
+	public TextField productPriceTextField;
+	public TextField productSerialIDTexField;
+	public TextField productNameTextField;
+	public TableView InventoryTable;
+	public TableColumn ValCol;
+	public TableColumn SerialNumCol;
+	public TableColumn NameCol;
+
+	public void addIInventoryClck(ActionEvent actionEvent)
+	{
+	}
 }
