07cce6d877e03cc42f900e74b433224e5ebb35f9
fshofiquel <fshofique@gmail.com>
2021-07-23 14:14:37 -0400

    Scrapped inserting data through a new windows due to the complexity and my small brain being unable to understand it. So using the method based on the todolist

--- src/main/java/ucf/assignments/TrackingInventoryController.java
+++ src/main/java/ucf/assignments/TrackingInventoryController.java
@@ -4,6 +4,8 @@
  */
 package ucf.assignments;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,9 +14,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Formatter;
 
 // This controller will allow the user to remove and edit each invetory item.
 // Since this is a TableView then there will be no need to impliment a sorting method as the TableView can do that
@@ -23,35 +31,69 @@ import java.io.IOException;
 // Likely need a FilterList to do this.
 public class TrackingInventoryController
 {
+	// Input variables
 	@FXML
-	private TextField searchTextArea;
+	private TextField searchTextField;
 	@FXML
-	public TableView InventoryTable;
+	private TextField valueTextField;
 	@FXML
-	public TableColumn ValCol;
+	private TextField serialNumberTextField;
 	@FXML
-	public TableColumn SerialNumCol;
+	private TextField nameTextField;
+
+	// Table elements
 	@FXML
-	public TableColumn NameCol;
+	public TableView<createInventory> inventoryTable;
+	@FXML
+	public TableColumn<createInventory, String> valueColumn;
+	@FXML
+	public TableColumn<createInventory, String> serialNumberColumn;
+	@FXML
+	public TableColumn<createInventory, String> nameColumn;
 
 
-	// This will open the window to the InputNewItemController
+	// Used as buffer to store strings obtained from the textfields and store them into TableView
+	public ObservableList<createInventory> bufferList = FXCollections.observableArrayList();
+	//Global variable used for the index of the tasks in table list and removing them.
+	private int index = -1;
+
+	//
 	@FXML
 	public void searchClick(ActionEvent actionEvent)
 	{
+	}
+
+	// Update the index value of the current index of the inventoryTable
+	@FXML
+	public void itemSelectClick(MouseEvent mouseEvent)
+	{
+		index = inventoryTable.getSelectionModel().getSelectedIndex();
 	}
 
 	// Removes and item that is selected from the TableView
 	@FXML
 	public void removeItemClick(ActionEvent actionEvent)
 	{
+		if (index != -1)
+		{
+			inventoryTable.getItems().remove(index);
+		}
 	}
 
 	// propmpt opening the InputNewItem window.
 	@FXML
 	public void addItemClick(ActionEvent actionEvent)
 	{
-		openNewWindow("InputNewItem.fxml", "Input New Item");
+		if (!(valueTextField.getText().trim().isEmpty()) &&
+				!(serialNumberTextField.getText().trim().isEmpty()) &&
+				!(nameTextField.getText().trim().isEmpty()))
+		{
+			//DecimalFormat formatter = new DecimalFormat("##.##");
+			//String formattedMoney = formatter.format(valueTextField.getText());
+			populateBuffer("text", serialNumberTextField.getText(), nameTextField.getText());
+			setTheCells();
+			inventoryTable.setItems(bufferList);
+		}
 	}
 
 	// Causes the button to launch a new window for save and import.
@@ -59,6 +101,45 @@ public class TrackingInventoryController
 	public void openSaveFileController(ActionEvent actionEvent)
 	{
 		openNewWindow("saveFileAs.fxml", "Save As / Import File");
+	}
+
+	@FXML
+	public void populateBuffer(String value, String serialNumber, String name)
+	{
+		bufferList.add(new createInventory(
+				value,
+				serialNumber,
+				name));
+	}
+
+	@FXML
+	public void setTheCells()
+	{
+		inventoryTable.setEditable(true);
+
+		valueColumn.setCellValueFactory(new PropertyValueFactory<>("value"));
+		valueColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+		valueColumn.setOnEditCommit(event ->
+		{
+			createInventory list = event.getRowValue();
+			list.setStatus(event.getNewValue());
+		});
+
+		serialNumberColumn.setCellValueFactory(new PropertyValueFactory<>("serialNumber"));
+		serialNumberColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+		serialNumberColumn.setOnEditCommit(event ->
+		{
+			createInventory list = event.getRowValue();
+			list.setStatus(event.getNewValue());
+		});
+
+		nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+		nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+		nameColumn.setOnEditCommit(event ->
+		{
+			createInventory list = event.getRowValue();
+			list.setStatus(event.getNewValue());
+		});
 	}
 
 	// Helper method to reduce clutting when a new window needs to be called.
@@ -77,6 +158,4 @@ public class TrackingInventoryController
 			e.printStackTrace();
 		}
 	}
-
-
 }
--- src/main/java/ucf/assignments/createInventory.java
+++ src/main/java/ucf/assignments/createInventory.java
@@ -4,8 +4,54 @@
  */
 package ucf.assignments;
 
-// This will be a getter, setter, putter class used to create each new invetory item, similar to how the ToDoList app
+// This will be a constructors, getter and setter class used to create each new invetory item, similar to how the ToDoList app
 // was setup.
 public class createInventory
 {
+	private String value;
+	private String serialNumber;
+	private String name;
+
+	// Contructors
+	public createInventory(String value, String serialNumber, String name)
+	{
+		this.value = value;
+		this.serialNumber = serialNumber;
+		this.name = name;
+	}
+
+	public void setStatus(String value)
+	{
+		this.value = value;
+	}
+
+	public String getValue()
+	{
+		return value;
+	}
+
+	public void setValue(String value)
+	{
+		this.value = value;
+	}
+
+	public String getSerialNumber()
+	{
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber)
+	{
+		this.serialNumber = serialNumber;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 }
